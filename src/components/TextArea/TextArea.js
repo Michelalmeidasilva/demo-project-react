3d20c145cf001447bdbf6d64c1152f4e00d8f8d8
Michel Silva <michelalmeidasilva@outlook.pt>
2021-05-20 11:12:58 -0300

    fix: some strange behaviors in the componets have been fixed

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -2,7 +2,7 @@ import { Column, Text } from 'components'
 import styled from 'styled-components'
 
 const TextArea = ({ name, label, register }) => (
-  <Column>
+  <Column mb='20px'>
     {label && <Text mb={1}>{label}</Text>}
     <TextAreaStyle id='pedido' required rows='5' {...register(name, { required: true })} />
   </Column>
@@ -12,6 +12,7 @@ const TextAreaStyle = styled.textarea`
    {
     width: 250px;
     height: 100px;
+    padding: 30px;
   }
 `
 export default TextArea
